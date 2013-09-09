47d8b84a487ee798d60ac1323c81051d697e1353
Andrea Danzi <andrea@danzi.tn.it>
2013-09-09 18:13:26 +0200

    Widget dei commenti per modulo Rumors
    Widget dei commenti per modulo Rumors

---
+++ modules/SDK/update_rumors_02.php
@@ -0,0 +1,19 @@
+<?php
+include_once('../../config.inc.php');
+chdir($root_directory);
+require_once('include/utils/utils.php');
+include_once('vtlib/Vtiger/Module.php');
+$Vtiger_Utils_Log = true;
+global $adb, $table_prefix;
+session_start();
+
+// Turn on debugging level
+$Vtiger_Utils_Log = true;
+include_once('vtlib/Vtiger/Menu.php');
+include_once('vtlib/Vtiger/Module.php');
+
+$rum = Vtiger_Module::getInstance('Rumors');
+require_once 'modules/ModComments/ModComments.php';
+$detailviewblock_nc = ModComments::addWidgetTo('Rumors');
+
+?>
